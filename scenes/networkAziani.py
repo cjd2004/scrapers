a41169117d06d788a706d82d58faaa0e72b855d6
cjd2004 <cjd2004@users.noreply.github.com>
2024-07-24 23:24:12 +1000

    Update networkAziani.py
    start work on api

--- scenes/networkAziani.py
+++ scenes/networkAziani.py
@@ -9,238 +9,39 @@ from tpdb.BaseSceneScraper import BaseSceneScraper
 from tpdb.items import SceneItem
 
 
-class ProjectOneServiceSpider(BaseSceneScraper):
-    name = 'ProjectOneService'
-    network = 'mindgeek'
-
-    custom_settings = {'CONCURRENT_REQUESTS': '4',
+class AzianiScraper(BaseSceneScraper):
+    name = 'Aziani'
+    network = 'nomadmedia'
+    cookies = {'X-NATS-cms-area-id': '3',}
+    start = 0
+'''
+    custom_settings = {'X-NATS-cms-area-id': '3',
                        # ~ 'AUTOTHROTTLE_ENABLED': 'True',
                        # ~ 'AUTOTHROTTLE_DEBUG': 'False',
                        # ~ 'DOWNLOAD_DELAY': '2',
-                       'CONCURRENT_REQUESTS_PER_DOMAIN': '2',
+                       # ~ 'CONCURRENT_REQUESTS_PER_DOMAIN': '2',
                        }
-
-    start_urls = [
-        # Only need one starting URL per "site", pulls everything through common feed
-
-        'https://www.babes.com',
-        # 'https://www.babesunleashed.com',
-        # 'https://www.elegantanal.com',
-        # 'https://www.officeobsession.com',
-        # 'https://www.stepmomlessons.com',
-
-        # 'https://www.bellesafilms.com', # Removed from Mind Geek connections
-        # 'https://www.bellesa.com',
-        # 'https://www.bellesahouse.com',
-
-        'https://www.bangbros.com',
-        'https://www.biempire.com',
-        'https://www.brazzers.com',
-        # 'https://www.babygotboobs.com',
-        # 'https://www.bigbuttslikeitbig.com',
-        # 'https://www.bigtitsatschool.com',
-        # 'https://www.bigtitsatwork.com',
-        # 'https://www.bigtitsinsports.com',
-        # 'https://www.bigtitsinuniform.com',
-        # 'https://www.bigwetbutts.com',
-        # 'https://www.brazzersexxtra.com',
-        # 'https://www.brazzersvault.com',
-        # 'https://www.bustyandreal.com',
-        # 'https://www.cfnm.com',
-        # 'https://www.daywithapornstar.com',
-        # 'https://www.dirtymasseur.com',
-        # 'https://www.doctoradventures.com',
-        # 'https://www.hotandmean.com',
-        # 'https://www.hotchicksbigasses.com',
-        # 'https://www.jugfuckers.com',
-        # 'https://www.milfslikeitbig.com',
-        # 'https://www.mommygotboobs.com',
-        # 'https://www.momsincontrol.com',
-        # 'https://www.pornstarslikeitbig.com',
-        # 'https://www.realwifestories.com',
-        # 'https://www.shesgonnasquirt.com',
-        # 'https://www.teenslikeitbig.com',
-        # 'https://www.zzseries.com',
-
-        'https://www.bromo.com',
-        # 'https://www.bromoblackmaleme.com',
-        # 'https://www.bromous.com',
-
-        'https://www.deviante.com',
-        # 'https://www.eroticspice.com',
-        # 'https://www.forgivemefather.com',
-        # 'https://www.loveherass.com',
-        # 'https://www.prettydirtyteens.com',
-        # 'https://www.sexworking.com',
-
-        'https://www.czechhunter.com/',
-        'https://www.digitalplayground.com',
-        'https://www.dilfed.com',
-        # 'https://www.gilfed.com', API responses from this site are borked
-        'https://www.erito.com',
-        'https://www.fakehub.com',
-        # 'https://www.fakeagent.com',
-        # 'https://www.fakeagentuk.com',
-        # 'https://www.fakecop.com',
-        # 'https://www.fakedrivingschool.com',
-        # 'https://www.fakehospital.com',
-        # 'https://www.fakehostel.com',
-        # 'https://www.faketaxi.com',
-        # 'https://www.fakehuboriginals.com',
-        # 'https://www.femaleagent.com',
-        # 'https://www.femalefaketaxi.com',
-        # 'https://www.publicagent.com',
-
-        'https://www.iconmale.com',
-        'https://www.men.com',
-        'https://www.metrohd.com',
-        # 'https://www.devianthardcore.com',
-        # 'https://www.familyhookups.com',
-        # 'https://www.girlgrind.com',
-        # 'https://www.kinkyspa.com',
-        # 'https://www.shewillcheat.com',
-
-        'https://www.milehighmedia.com',
-        # 'https://www.doghousedigital.com',
-        # 'https://www.familysinners.com',
-        # 'https://www.realityjunkies.com',
-        # 'https://www.sweetheartvideo.com',
-        # 'https://www.sweetsinner.com',
-
-        'https://www.milfed.com',
-        # 'https://www.cherrypop.com',
-        # 'https://www.doghousedigital.com',    (Crossover with MileHighMedia)
-        # 'https://www.familysinners.com',      (Crossover with MileHighMedia)
-        # 'https://www.lesbianolderyounger.com',
-        # 'https://www.milehighmedia.com',      (Crossover with MileHighMedia)
-        # 'https://www.realityjunkies.com',     (Crossover with MileHighMedia)
-        # 'https://www.sweetheartvideo.com',    (Crossover with MileHighMedia)
-        # 'https://www.sweetsinner.com',        (Crossover with MileHighMedia)
-
-        'https://www.mofos.com',
-        # 'https://www.bustedbabysitters.com',
-        # 'https://www.dontbreakme.com',
-        # 'https://www.dronehunter.com',
-        # 'https://www.girlsgonepink.com',
-        # 'https://www.iknowthatgirl.com',
-        # 'https://www.ingangwebang.com',
-        # 'https://www.latinasextapes.com',
-        # 'https://www.letstryanal.com',
-        # 'https://www.milfslikeitblack.com',
-        # 'https://www.mofosbsides.com',
-        # 'https://www.mofoslab.com',
-        # 'https://www.mofosworldwide.com',
-        # 'https://www.pervsonpatrol.com',
-        # 'https://www.pornstarvote.com',
-        # 'https://www.projectrv.com',
-        # 'https://www.publicpickups.com',
-        # 'https://www.realslutparty.com',
-        # 'https://www.sharemybf.com',
-        # 'https://www.shesafreak.com',
-        # 'https://www.strandedteens.com',
-        # 'https://www.thesexscout.com',
-
-        'https://www.noirmale.com',
-        'https://www.propertysex.com',
-        'https://www.realitydudesnetwork.com',
-        'https://www.realitykings.com',
-        # 'https://www.40inchplus.com',
-        # 'https://www.8thstreetlatinas.com',
-        # 'https://www.badtowtruck.com',
-        # 'https://www.bignaturals.com',
-        # 'https://www.bigtitsboss.com',
-        # 'https://www.captainstabbin.com',
-        # 'https://www.cfnmsecret.com',
-        # 'https://www.crazyasiangfs.com',
-        # 'https://www.crazycollegegfs.com',
-        # 'https://www.cumfiesta.com',
-        # 'https://www.daredorm.com',
-        # 'https://www.eurosexparties.com',
-        # 'https://www.extremeasses.com',
-        # 'https://www.extremenaturals.com',
-        # 'https://www.firsttimeauditions.com',
-        # 'https://www.gfrevenge.com',
-        # 'https://www.girlsofnaked.com',
-        # 'https://www.happytugs.com',
-        # 'https://www.hdlove.com',
-        # 'https://www.hornybirds.com',
-        # 'https://www.hotbush.com',
-        # 'https://www.inthevip.com',
-        # 'https://www.lilhumpers.com',
-        # 'https://www.lookathernow.com',
-        # 'https://www.mikeinbrazil.com',
-        # 'https://www.mikesapartment.com',
-        # 'https://www.milfhunter.com',
-        # 'https://www.milfnextdoor.com',
-        # 'https://www.momsbangteens.com',
-        # 'https://www.momslickteens.com',
-        # 'https://www.moneytalks.com',
-        # 'https://www.monstercurves.com',
-        # 'https://www.nofaces.com',
-        # 'https://www.pure18.com',
-        # 'https://www.recklessinmiami.com',
-        # 'https://www.rkprime.com',
-        # 'https://www.roundandbrown.com',
-        # 'https://www.saturdaynightlatinas.com',
-        # 'https://www.seemywife.com',
-        # 'https://www.sneakysex.com',
-        # 'https://www.streetblowjobs.com',
-        # 'https://www.teenslovehugecocks.com',
-        # 'https://www.welivetogether.com',
-        # 'https://www.wivesinpantyhose.com',
-
-        'https://www.seancody.com',
-        'https://www.sexselector.com',
-        'https://www.sexyhub.com',
-        # 'https://www.danejones.com',
-        # 'https://www.fitnessrooms.com',
-        # 'https://www.girlfriends.com',
-        # 'https://www.lesbea.com',
-        # 'https://www.massagerooms.com',
-        # 'https://www.momxxx.com',
-
-        'https://www.squirted.com',
-        'https://www.thegayoffice.com',
-        # 'https://www.bigdicksatschool.com',
-        # 'https://www.bromous.com', (Crossover with Bromo)
-        # 'https://www.drillmyhole.com',
-        # 'https://www.godsofmen.com',
-        # 'https://www.jizzorgy.com',
-        # 'https://www.menofuk.com',
-        # 'https://www.realitydudes.com',
-        # 'https://www.str8chaser.com',
-        # 'https://www.str8togay.com',
-        # 'https://www.toptobottom.com',
-
-        'https://www.transangelsnetwork.com',
-        # 'https://www.transangels.com',
-
-        'https://www.transharder.com',  # Seems to be the same as TransAngels, but some additional
-        'https://www.transsensual.com',  # Seems to be the same as TransAngels, but some additional
-        'https://www.trueamateurs.com',
-        'https://www.tube8vip.com',
-        'https://www.twistys.com',
-        # 'https://www.anettedawn.com',
-        # 'https://www.featurefilm.com',
-        # 'https://www.momknowsbest.com',
-        # 'https://www.nicolegraves.com',
-        # 'https://www.turningtwistys.com',
-        # 'https://www.twistysteasers.com',
-        # 'https://www.twistyshard.com',
-        # 'https://www.whengirlsplay.com',
-
-        'https://www.voyr.com',
-        'https://www.whynotbi.com',
-    ]
-
-    selector_map = {
-        'external_id': 'scene\\/(\\d+)'
-    }
 
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse, headers=self.headers, cookies=self.cookies,
                                  meta={'url': url})
+                       
+'''
+    start_urls = [
+        # Only need one starting URL per "site", pulls everything through common feed
+
+        'https://azianistudios.com',
+
+    ]
+
+    selector_map = {
+        'external_id': 'scene\\/(\\d+)'
+        'pagination':tour_api.php/content/sets?cms_set_ids=&data_types=1&content_count=1&count=48&start=%s&cms_area_id=3&cms_block_id=106775&orderby=published_desc&content_type=video&status=enabled'
+
+
+    def get_next_page_url(self, base, page):
+        return self.format_url(base, self.get_selector_map('pagination') % start)
 
     def parse(self, response):
         token = self.get_token(response)
@@ -260,7 +61,7 @@ class ProjectOneServiceSpider(BaseSceneScraper):
     def get_scenes(self, response):
         scene_count = 0
 
-        for scene in response.json()['result']:
+        for scene in response.json()['sets']:
             item = SceneItem()
             if scene['collections'] and len(scene['collections']):
                 item['site'] = scene['collections'][0]['name']
@@ -286,7 +87,7 @@ class ProjectOneServiceSpider(BaseSceneScraper):
             item['network'] = self.network
             item['parent'] = tldextract.extract(response.meta['url']).domain
 
-            if 'title' in scene:
+            if 'name' in scene:
                 item['title'] = scene['title']
             else:
                 item['title'] = item['site'] + ' ' + self.parse_date(scene['dateReleased']).strftime('%b/%d/%Y')
